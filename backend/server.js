d7eba4872714ceb5f10684e012108734df3d076c
Latika Rai <lr5370566@gmail.com>
2026-07-07 19:31:55 +0530

    fix Railway start script

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,8 @@ io.on('connection', (Socket)=>{
 
 })
 
-httpServer.listen(3000, ()=>{
-    console.log('server is running on port 3000')
+const PORT = process.env.PORT || 3000
+
+httpServer.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
 })

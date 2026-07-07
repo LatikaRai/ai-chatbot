27ddd888f59425829990b528b7edf16f50386b6e
Latika Rai <lr5370566@gmail.com>
2026-07-07 22:49:24 +0530

    adding cors in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const cors = require("cors");
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://ai-chatbot-eight-chi-20.vercel.app",
+    "https://ai-chatbot-lq2r2dkdh-zen-stack.vercel.app",
+  ],
+  credentials: true,
+}));
 
 module.exports = app
